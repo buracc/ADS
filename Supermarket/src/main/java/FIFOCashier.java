56fc67b903cbf884d171a88a06d8c65e2acead04
Safak <safak.inan61@hotmail.com>
2019-10-17 16:37:55 +0200

    mk

---
+++ Supermarket/src/main/java/FIFOCashier.java
@@ -0,0 +1,23 @@
+import java.time.LocalTime;
+
+public class FIFOCashier extends Cashier {
+
+    protected FIFOCashier(String name) {
+        super(name);
+    }
+
+    @Override
+    public int expectedCheckOutTime(int numberOfItems) {
+        return 0;
+    }
+
+    @Override
+    public int expectedWaitingTime(Customer customer) {
+        return 0;
+    }
+
+    @Override
+    public void doTheWorkUntil(LocalTime targetTime) {
+
+    }
+}
---
+++ Supermarket/src/main/java/PriorityCashier.java
@@ -0,0 +1,6 @@
+public class PriorityCashier extends FIFOCashier{
+
+    protected PriorityCashier(String name) {
+        super(name);
+    }
+}
